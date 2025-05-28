b613d2e031570e445a3c840fce414208a51f957a
geraldy.miero <geraldy.miero@maucash.id>
2025-05-28 16:32:42 +0700

    update: add chart

---
+++ src/components/ChartComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <h1>Chart Component</h1>
+    <div style="width: 400px; height: 400px;">
+        <canvas id="myChart"></canvas>
+    </div>
+</template>
+<script>
+import Chart from 'chart.js/auto';
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June']
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'My first datasets',
+        backgroundColor: ['rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)', 'rgb(69, 99, 169)',
+      'rgb(54, 162, 255)',
+      'rgb(255, 205, 69)'],
+        hoverOffset: 4,
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }],
+}
+
+const config = {
+    type: 'doughnut',
+    data: data,
+    options: {},
+}
+export default {
+    name: 'ChartComponent',
+    mounted() {
+        const myChart = new Chart(document.getElementById('myChart'), config)
+    },
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="home">
-    <h1>INI HOME</h1>
+    <h1>BERANDA</h1>
     <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo adipisci dolore possimus velit consequatur ullam perferendis commodi animi! Minima suscipit vel repudiandae eum non, voluptatibus maiores itaque laudantium sit harum!</span>
     <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo adipisci dolore possimus velit consequatur ullam perferendis commodi animi! Minima suscipit vel repudiandae eum non, voluptatibus maiores itaque laudantium sit harum!</span>
     <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo adipisci dolore possimus velit consequatur ullam perferendis commodi animi! Minima suscipit vel repudiandae eum non, voluptatibus maiores itaque laudantium sit harum!</span>
+    <div>
+      <ChartComponent />
+    </div>
   </div>
 </template>
 
 <script>
+import ChartComponent from '@/components/ChartComponent.vue';
+
 export default {
   name: 'HomeView',
-  components: {},
+  components: {
+    ChartComponent,
+  },
+  data() {
+    return {}
+  },
 }
 </script>
