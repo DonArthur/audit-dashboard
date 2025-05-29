2c85bff81eb517bbac8dc40498ab6d16bd65254b
geraldy.miero <geraldy.miero@maucash.id>
2025-05-29 14:59:39 +0700

    udpate: remove lorem

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="home">
     <h1>BERANDA</h1>
-    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo adipisci dolore possimus velit consequatur ullam perferendis commodi animi! Minima suscipit vel repudiandae eum non, voluptatibus maiores itaque laudantium sit harum!</span>
-    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo adipisci dolore possimus velit consequatur ullam perferendis commodi animi! Minima suscipit vel repudiandae eum non, voluptatibus maiores itaque laudantium sit harum!</span>
-    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo adipisci dolore possimus velit consequatur ullam perferendis commodi animi! Minima suscipit vel repudiandae eum non, voluptatibus maiores itaque laudantium sit harum!</span>
     <div>
       <ChartComponent />
     </div>
